32923d46b0c642ad6d75003ad02bce4dad7c5889
pallav-trilio <pallav.mathur@trilio.io>
2021-05-14 16:35:41 +0530

    :seedling: Added Binary Path for tvm-upgrade

---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+cd $HELM_PLUGIN_DIR
+version="$(cat plugin.yaml | grep "version" | cut -d '"' -f 2)"
+echo "Installing tvm-upgrade ${version} ..."
+
+# Find correct archive name
+unameOut="$(uname -s)"
+
+case "${unameOut}" in
+    Linux*)             os=Linux;;
+    Darwin*)            os=Darwin;;
+    MINGW*|MSYS_NT*)    os=windows;;
+    *)                  os="UNKNOWN:${unameOut}"
+esac
+
+arch=`uname -m`
+
+if echo "$os" | grep -qe '.*UNKNOWN.*'
+then
+    echo "Unsupported OS / architecture: ${os}_${arch}"
+    exit 1
+fi
+
+url="https://github.com/pallav-trilio/tvm-helm-plugins/blob/main/dist/tvm-upgrade_v0.0.0_linux_amd64.tar.gz"
+
+filename=`echo ${url} | sed -e "s/^.*\///g"`
+
+  # Download archive
+  if [ -n $(command -v curl) ]
+  then
+      curl -sSL -O $url
+  elif [ -n $(command -v wget) ]
+  then
+      wget -q $url
+  else
+      echo "Need curl or wget"
+      exit -1
+  fi
+
+# Install bin
+rm -rf bin && mkdir bin && tar xvf $filename -C bin > /dev/null && rm -f $filename
