bc641b2eba9843f8c41cb9aa47f7bb01d8f2737b
pallav-trilio <pallav.mathur@trilio.io>
2021-05-15 18:32:49 +0530

    :seedling: Added Fallback arch and OS case

--- install.sh
+++ install.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-
-cd $HELM_PLUGIN_DIR
-version="$(cat plugin.yaml | grep "version" | cut -d '"' -f 2)"
+cd "$HELM_PLUGIN_DIR" || exit
+version="$(grep  "version" plugin.yaml | cut -d '\"' -f 2)"
 echo "Installing tvm-upgrade ${version} ..."
-
 # Find correct archive name
 unameOut="$(uname -s)"
-
 case "${unameOut}" in
     Linux*)             os=linux;;
     Darwin*)            os=darwin;;
@@ -14,41 +11,32 @@ case "${unameOut}" in
     Windows_NT)         os=windows;;
     *)                  os="UNKNOWN:${unameOut}";;
 esac
-
-archOut=`uname -m`
-
+archOut=$(uname -m)
 case "${archOut}" in
     amd64*)             arch=amd64;;
     x86_64*)            arch=amd64;;
     386*)               arch=386;;
     *)                  arch=386;;
 esac
-
 if echo "$os" | grep -qe '.*UNKNOWN.*'
 then
     echo "Unsupported OS / architecture: ${os}_${arch}"
     exit 1
 fi
-
 echo "OS/Arch : ${os}/$(arch)"
-
 url="https://github.com/trilioData/tvm-helm-plugins/blob/main/dist/tvm-upgrade_v0.0.0_${os}_${arch}.tar.gz?raw=true"
-
-filename=`echo ${url} | sed -e "s/^.*\///g"`
-
+filename=$(echo "${url}" | sed -e "s/^.*\///g")
   # Download archive
-  if [ -n $(command -v curl) ]
+  if [[ -n "$(command -v curl)" ]]
   then
-      curl -sSL -O $url
-  elif [ -n $(command -v wget) ]
+      curl -sSL -O "$url"
+  elif [[ -n "$(command -v wget)" ]]
   then
-      wget -q $url
+      wget -q "$url"
   else
       echo "Need curl or wget"
-      exit -1
+      exit 1
   fi
-
 echo "Downloaded Binary"
-
 # Install bin
-rm -rf bin && mkdir bin && tar xvf $filename -C bin > /dev/null && rm -f $filename
+rm -rf bin && mkdir bin && tar xvf "$filename" -C bin > /dev/null && rm -f "$filename"
