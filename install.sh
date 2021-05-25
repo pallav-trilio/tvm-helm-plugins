a458e5316a88ed0faa250a7bf04fcaf73d87da9f
pallav-trilio <pallav.mathur@trilio.io>
2021-05-25 19:32:57 +0530

    :seedling: plugin-test

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env sh
-
 cd "$HELM_PLUGIN_DIR" || exit
 version="$(grep  "version" plugin.yaml | cut -d '"' -f 2)"
 echo "Installing tvm-upgrade ${version} ..."
-
 # Find correct archive name
 unameOut="$(uname -s)"
 case "${unameOut}" in
@@ -11,15 +9,12 @@ case "${unameOut}" in
     Darwin*)            os=darwin;;
     *)                  os=windows;;
 esac
-
 archOut=$(uname -m)
 case "${archOut}" in
     amd64*)             arch=amd64;;
     x86_64*)            arch=amd64;;
     *)                  arch=386;;
 esac
-
-
 echo "OS/Arch : ${os}/${arch}"
 url="https://github.com/pallav-trilio/tvm-helm-plugins/blob/main/dist/tvm-upgrade_v0.0.0_${os}_${arch}.tar.gz?raw=true"
 filename=$(echo "${url}" | sed -e "s/^.*\///g" | awk -F "?" '{print $1}')
